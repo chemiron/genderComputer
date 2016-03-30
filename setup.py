b1df10264ed04de9d18e788f5f5505d24a8109d5
chemiron <chemiron34@gmail.com>
2016-03-30 12:18:52 +0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='genderComputer',
+      version='0.0.1',
+      description="Python tool that tries to infer a person's gender from their name (mostly first name) and location (country)",
+      long_description=open('readme.md').read(),
+      author='Alexander Serebrenik',
+      author_email='a.serebrenik tue.nl',
+      packages=find_packages(),
+      install_requires=["nameparser", "unidecode"],
+      classifiers=[
+          'Development Status :: Beta',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Topic :: Utilities',
+      ],
+)
